2952ffb51bcc8f528ea901aa929968a2a0ba2c6e
Cybarsan <alan.sanier@gmail.com>
2024-12-07 22:13:01 -0300

    Primer Commit

---
+++ src/main/java/seek/com/retocodigo/RetocodigoApplication.java
@@ -0,0 +1,13 @@
+package seek.com.retocodigo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetocodigoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetocodigoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/seek/com/retocodigo/RetocodigoApplicationTests.java
@@ -0,0 +1,13 @@
+package seek.com.retocodigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetocodigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
